c030f51dc899324eb8e93a33cde0409f76f554fd
GeekTony <allabttony@gmail.com>
2015-11-15 22:59:37 +0800

    Initial Commit

---
+++ CCKCameraCore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCKCameraCore
+//
+//  Created by Tony on 11/15/15.
+//  Copyright © 2015 tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
